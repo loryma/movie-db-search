a8bc07d9ea62b4cff6631a9a747a62f4ebbd2de3
loryma <lorymalloc@gmail.com>
2020-04-13 23:37:37 +0300

    create search item component

--- src/scenes/search/results/Results.js
+++ src/scenes/search/results/Results.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { Card, Icon, Grid, Image } from "semantic-ui-react";
-import { Link } from "react-router-dom";
+import { Grid } from "semantic-ui-react";
 import defaultImage from "./not-found.png";
+import ResultItem from "../resultItem";
 import "./Results.css";
 
 const base_url = "http://image.tmdb.org/t/p/";
@@ -10,39 +10,13 @@ const backdrop_sizes = ["w300", "w780", "w1280", "original"];
 function Results({ results }) {
   return (
     <Grid doubling columns={4}>
-      {results.map(
-        ({ id, title, poster_path, release_date, overview, vote_average }) => {
-          const src = poster_path
-            ? `${base_url}${backdrop_sizes[0]}/${poster_path}`
-            : defaultImage;
+      {results.map(({ id, poster_path, ...rest }) => {
+        const src = poster_path
+          ? `${base_url}${backdrop_sizes[0]}/${poster_path}`
+          : defaultImage;
 
-          return (
-            <Grid.Column key={id}>
-              <Card
-                as={Link}
-                to={`movies/${id}`}
-                fluid
-                className="results__card"
-              >
-                <Image src={src} wrapped ui={false} />
-                <Card.Content>
-                  <Card.Header>{title}</Card.Header>
-                  <Card.Meta>
-                    <span className="date">Released: {release_date}</span>
-                  </Card.Meta>
-                  <Card.Description className="results__description">
-                    {overview}
-                  </Card.Description>
-                </Card.Content>
-                <Card.Content extra>
-                  <Icon name="star" />
-                  {vote_average}
-                </Card.Content>
-              </Card>
-            </Grid.Column>
-          );
-        }
-      )}
+        return <ResultItem key={id} id={id} src={src} {...rest} />;
+      })}
     </Grid>
   );
 }
