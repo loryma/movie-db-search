b8f72d6b89295c4c9c9a049a7259f263d3c35d89
loryma <lorymalloc@gmail.com>
2020-04-13 22:32:10 +0300

    add styles for vides in search results

--- src/scenes/search/results/Results.js
+++ src/scenes/search/results/Results.js
@@ -1,37 +1,47 @@
 import React from "react";
 import { Card, Icon, Grid, Image } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+import defaultImage from "./not-found.png";
+import "./Results.css";
 
 const base_url = "http://image.tmdb.org/t/p/";
 const backdrop_sizes = ["w300", "w780", "w1280", "original"];
 
 function Results({ results }) {
   return (
-    <Grid doubling columns={5}>
+    <Grid doubling columns={4}>
       {results.map(
-        ({ id, title, poster_path, release_date, overview, vote_average }) => (
-          <Grid.Column>
-            <Card key={id}>
-              <Image
-                src={`${base_url}/${backdrop_sizes[0]}/${poster_path}`}
-                wrapped
-                ui={false}
-              />
-              <Card.Content>
-                <Card.Header>{title}</Card.Header>
-                <Card.Meta>
-                  <span className="date">Released: {release_date}</span>
-                </Card.Meta>
-                <Card.Description>{overview}</Card.Description>
-              </Card.Content>
-              <Card.Content extra>
-                <a>
+        ({ id, title, poster_path, release_date, overview, vote_average }) => {
+          const src = poster_path
+            ? `${base_url}${backdrop_sizes[0]}/${poster_path}`
+            : defaultImage;
+
+          return (
+            <Grid.Column key={id}>
+              <Card
+                as={Link}
+                to={`movies/${id}`}
+                fluid
+                className="results__card"
+              >
+                <Image src={src} wrapped ui={false} />
+                <Card.Content>
+                  <Card.Header>{title}</Card.Header>
+                  <Card.Meta>
+                    <span className="date">Released: {release_date}</span>
+                  </Card.Meta>
+                  <Card.Description className="results__description">
+                    {overview}
+                  </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
                   <Icon name="star" />
                   {vote_average}
-                </a>
-              </Card.Content>
-            </Card>
-          </Grid.Column>
-        )
+                </Card.Content>
+              </Card>
+            </Grid.Column>
+          );
+        }
       )}
     </Grid>
   );
