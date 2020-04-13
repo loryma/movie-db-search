db611b81a9b23d559d74758f48ff4eea3d2b2bb2
loryma <lorymalloc@gmail.com>
2020-04-13 14:51:31 +0300

    add redux store, reducer

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,19 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import Search from "scenes/search";
 import Movie from "scenes/movie";
 
 const routes = (
-  <Switch>
-    <Route path="/movies/:id">
-      <Movie />
-    </Route>
-    <Route path="/">
-      <Search />
-    </Route>
-  </Switch>
+  <Router>
+    <Switch>
+      <Route path="/movies/:id">
+        <Movie />
+      </Route>
+      <Route path="/">
+        <Search />
+      </Route>
+    </Switch>
+  </Router>
 );
 
 export default routes;
