7c5660e434d92565bba1619d985d8da7903a86a8
loryma <lorymalloc@gmail.com>
2020-04-22 02:24:24 +0300

    add profiler and use interaction tracing

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import "./index.css";
 import "semantic-ui-css/semantic.min.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import { unstable_trace as trace } from "scheduler/tracing";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+trace("initial render", performance.now(), () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+});
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
